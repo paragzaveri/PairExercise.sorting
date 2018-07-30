ac8a35af0de4e28cd4c800c2c3bd01d8133bb5f8
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-07-30 10:47:59 -0500

    fills bubble sort fucntion in .js file

---
+++ bubblesort.js
@@ -0,0 +1,17 @@
+var bubbleSort = function(array){
+  for (var i = 0; i < array.length; i++){
+    for (var j = i; j < array.length; j++){ //array.lenght -1 ?
+      if(array[j] > array[j+1]){
+        swap(array, j, j+1);
+      }
+    }
+  }
+  return array;
+};
+
+var swap = function(array, num1, num2){
+  var temp = array[num1];
+  array[num1] = array[num2];
+  array[num2] = temp;
+  return array;
+}
