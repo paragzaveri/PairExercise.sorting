11926f2a03608564c62997e1d192dabdfb2b4a97
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-07-30 10:40:26 -0500

    Adds Basic Functinality Tests

---
+++ bubblesort.spec.js
@@ -0,0 +1,8 @@
+describe('Bubble Sort', function(){
+  it('handles an empty array', function(){
+    expect( bubbleSort[] ).toEqual( [] );
+  });
+  it('handles a numerical array', function(){
+    expect( bubbleSort[4,2,7,3,9,5] ).toEqual( [2,3,4,5,7,9] )
+  });
+});
