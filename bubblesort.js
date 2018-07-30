493eb53385a79da84d5fa213933d4bb97312f963
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-07-30 11:47:29 -0500

    adds comparator function

--- bubblesort.js
+++ bubblesort.js
@@ -2,7 +2,7 @@ var bubbleSort = function(array){
   // let x = 0;
   for (var i = 0; i < array.length - 1; i++){
     for (var j = 0; j < array.length-i-1; j++){
-      if(array[j] > array[j+1]){
+      if(comparator(array[j], array[j+1])){
         swap(array, j, j+1);
         // x +=1;
 
@@ -18,4 +18,9 @@ var swap = function(array, num1, num2){
   array[num1] = array[num2];
   array[num2] = temp;
   return array;
+}
+
+var comparator = function (a, b) {
+  if (a > b) return 1;
+  if (a <= b) return 0;
 }
